97d48f362c853eb47051b213ab40be251c119001
xtaltong <ctong011@ucr.edu>
2020-10-21 22:40:18 -0700

    deallocated in compute_relation and printLineage

--- person.cpp
+++ person.cpp
@@ -54,6 +54,7 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
+    delete[] temp;
 }
 
 /* helper function to compute the lineage
@@ -69,7 +70,9 @@ char* Person::compute_relation(int level){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
         temp = temp2;
+        delete[] temp2;
     }
+
     return temp;
 }
 
